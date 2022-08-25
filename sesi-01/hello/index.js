5062f7eb84cc87b1008d3c95812d22dea70eb3ca
ellabw <111202012716@mhs.dinus.ac.id>
2022-08-25 09:00:37 +0700

    first commit

---
+++ sesi-01/hello/index.js
@@ -0,0 +1,37 @@
+alert("Halo Salam kenal!");
+alert("Selamat datang");
+
+let playerName = "Ella Budi Wijayanti";
+alert(playerName);
+playerName = "Ella";
+alert(playerName);
+
+let message = "Hactive8";
+alert(message);
+message = "react and react native";
+alert(message);
+message = "semangat belajar!!!";
+alert(message);
+
+
+const pi = 3.14
+// pi = 12
+console.log(pi);
+
+let score = 50
+console.log("Hasil variabel Score "+score);
+
+const objectAngkaKeb = {id:1, name:'Ella', jenkel:'P'}
+objectAngkaKeb.id = 2
+objectAngkaKeb.name = "ella"
+objectAngkaKeb.jenkel = "Perempuan"
+objectAngkaKeb.alamat = "Demak"
+console.log(objectAngkaKeb);
+
+const arrayNumber = [1,2,3,4,5]
+arrayNumber.push(6)
+arrayNumber.push(7)
+console.log(arrayNumber);
+
+var _nama = "test"
+ 
---
+++ sesi-01/kesimpulan/kesimpulan.js
@@ -0,0 +1,41 @@
+let variable = "Variabel adalah suatu yang berisi data dan nilai serta untuk menyimpan data atau nilai.";
+console.log(variable); //apa itu variabel
+
+let importantVar = "Variabel digunakan untuk menyimpan data atau nilai yang dimasukkan ke program";
+console.log(importantVar); //pentingnya variabel
+
+let deklarasiVar = "Memasukan data atau nilai dengan menggunakan var, let, dan cons";
+console.log(deklarasiVar); //mendeklrasikan variabel
+
+let isiVar = "mengisi variabel dengan berisikan data atau nilai setelah tanda ( = )";
+console.log(isiVar); //mengisi variable dengan nilai value
+
+let namaVar = "dapat diisikan dengan Nama variabel yang berisi huruf, angka, garis bawah dan tanda dollar ($)";
+console.log(namaVar); // Memberi nama variable yang baik
+
+let tipeData = "Tipe Data adalah sesuatu data yang digunakan dalam pemrograman untuk mengklasifikasi data berdasarkan jenis data tersebut";
+console.log(tipeData); // apa itu tipe data
+
+let jenistipDat = "jenis tipe data dalam javascript yaitu String, Number, Float, Boolean, Object, Array";
+console.log(jenistipDat); // berbagai jenis tipe data
+
+let tipeDataNumber = "tipe data yang berisi Number atau angka. caranya yaitu memasukan data atau nilai angka kedalam variabel tersebut";
+console.log(tipeDataNumber); // Tipe data Number
+
+let tipeDataString = "tipe data yang berisi teks dengan diawali dan diakhiri tanda kutip";
+console.log(tipeDataString); // Tipe data String
+
+let tipeDataBoolean = "tipe data yang hanya berisi 2 value yaitu true (benar) dan false (salah)";
+console.log(tipeDataBoolean);// Tipe data Boolean
+
+let dataType = "cara mengecek tipe data dari sebuah variabel yaitu dengan cara menggunakan fungsi (typeof)";
+console.log(dataType); // Cara mengetahui tipe data dari sebuah variable
+
+let array = "Tipe data array adalah tipe data yang bisa digunakan untuk menyimpan sebuah data";
+console.log(array); // Apa itu Array
+
+let jenisArray = "Jenis Array di javascript ada 3 yaitu Array Numerik, Array Assosiatif, Array Campuran";
+console.log(jenisArray); // Jenis Array
+
+let isiArray = "Cara mengisi data pada array yaitu yang pertama adalah kita buat variable yang akan diisi array setelah itu kita isi variable tersebut dengan [] dan isi didalamnya sebuah data";
+console.log(isiArray); // Mengisi Array
