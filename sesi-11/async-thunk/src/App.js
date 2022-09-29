e23dfbc90da7b261ffdca1487a41da30fbdf5374
ellabw <111202012716@mhs.dinus.ac.id>
2022-09-29 17:36:04 +0700

    commit

---
+++ sesi-11/async-thunk/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UsersList from './features/UsersList';
+
+function App () {
+  return (
+    <div className="App container">
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
